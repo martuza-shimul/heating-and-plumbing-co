5fb4d2bb64d7f4103ee75320c9a9759940416464
Martuza Shimul <martuza.shimul@gmail.com>
2019-11-13 21:29:03 +0600

    ver 2.0.0

--- data.php
+++ data.php
@@ -1,7 +1,7 @@
 <?php
 // Check for empty fields
 if(empty($_POST['first-name'])  		||
-   empty($_POST['last-name']) 		||
+   empty($_POST['phone']) 		||
    empty($_POST['email'])	||
    empty($_POST['postcode']) 		||
    
@@ -13,22 +13,33 @@ if(empty($_POST['first-name'])  		||
 
    
 $first = $_POST['first'];
-// $second = $_POST['check'];
+$second = $_POST['check'];
+$fixture = implode(', ', $second);
+
 $type = $_POST['type'];
 $third = $_POST['third'];
 $fourth = $_POST['fourth'];
 
+
+// if(!empty($second)) {
+
+//    foreach($_POST['lang'] as $value){
+//        echo "value : ".$value.'<br/>';
+//    }
+
+// }
+
 $first_name = $_POST['first-name'];
-$last_name = $_POST['last-name'];
+$phone = $_POST['phone'];
 $email_address = $_POST['email'];
 $postcode = $_POST['postcode'];
 
 	
 // Create the email and send the message
 
-$to = 'gmshimul1@gmail.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
+$to = 'tripkeys@gmail.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
 $email_subject = "Website Quote Engine:  $first_name";
-$email_body = "You have received a new message from your website Quote Engine .\n\n"."Here are the details:\n\nFirst-Name: $first_name\n\nLast-Name: $last_name\n\nEmail: $email_address\n\n Postcode: $postcode \n\n Here is the Quote Request: \n Type of Work: $first \n Type of Property: $type \n Timeline: $third \n Materials: $fourth ";
+$email_body = "You have received a new message from your website Quote Engine .\n\n"."Here are the details:\n\nName: $first_name\n\nPhone: $phone\n\nEmail: $email_address\n\n Postcode: $postcode \n\n Here is the Quote Request: \n Type of Work: $first\n Fixture/Appliance: $fixture \n Type of Property: $type \n Timeline: $third \n Materials: $fourth ";
 $headers = "From: noreply@eastsidebs.co.uk\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
 
 $headers .= "Reply-To: $email_address";	
